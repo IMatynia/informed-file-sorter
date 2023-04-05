1750f3489998516f76e111b6c8196b932d7b774e
Igor Matynia <igor.matynia@gmail.com>
2023-04-05 17:55:49 +0200

    destination panel UI and logic

--- src/ui/destination_folder_view.py
+++ src/ui/destination_folder_view.py
@@ -1,6 +1,8 @@
 import os.path
 
 from PySide6 import QtCore
+from PySide6.QtCore import QKeyCombination
+from PySide6.QtGui import QShortcut
 from PySide6.QtWidgets import QWidget
 from src.ui.ui_destination_folder import Ui_destinationFolder
 
@@ -9,7 +11,7 @@ class DestinationFolderView(QWidget):
     onClickWPath = QtCore.Signal(str)
     onRemoveWPath = QtCore.Signal(str)
 
-    def __init__(self, folder_path: str, highlight: bool = False):
+    def __init__(self, folder_path: str, highlight: bool = False, index: int = None):
         super().__init__()
         self._ui = Ui_destinationFolder()
         self._ui.setupUi(self)
@@ -18,11 +20,21 @@ class DestinationFolderView(QWidget):
         self._ui.btAdd.clicked.connect(self.on_add)
         self._ui.btRemove.clicked.connect(self.on_remove)
 
-        if highlight:
-            self._ui.btAdd.setDisabled(True)
-            self._ui.lbName.setText(f" > {os.path.basename(folder_path)} < ")
+        self._shortcut = None
+
+        number = None
+        if index is not None and index < 10:
+            number = (index + 1) % 10
+            self._shortcut = QShortcut(self)
+            self._shortcut.setKey(str(number))
+            self._shortcut.activated.connect(self.on_add)
+            self._ui.lbName.setText(f"[{number}] {os.path.basename(folder_path)}")
         else:
             self._ui.lbName.setText(os.path.basename(folder_path))
+
+        if highlight:
+            self._ui.lbName.setStyleSheet("font-weight: bold; text-decoration: underline")
+            self._ui.btAdd.setStyleSheet("background: #555555")
 
     def on_add(self):
         self.onClickWPath.emit(self._folder_path)
